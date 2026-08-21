cfa923e874c8f646d173f2fdedfa282586d1c4e4
Abhinav Mishra <153204383+abhinav-0608@users.noreply.github.com>
2026-08-21 18:20:43 +0300

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hashmap_s = {}
+        hashmap_t = {}
+
+        for i in range(len(s)):
+            hashmap_s[s[i]] = hashmap_s.get(s[i], 0) + 1
+            hashmap_t[t[i]] = hashmap_t.get(t[i], 0) + 1
+        
+        if hashmap_s != hashmap_t:
+            return False
+        else:
+            return True
+
